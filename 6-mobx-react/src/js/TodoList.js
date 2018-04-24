05c82fa2ab73d079c83763522c1a9181fba17510
Adam Miller <millera@Adam-Millers-MacBook-Pro.local>
2018-04-23 18:05:39 -0700

    Complete MobX Tutorial #1 and #2 from LearnCode.academy

--- 6-mobx-react/src/js/TodoList.js
+++ 6-mobx-react/src/js/TodoList.js
@@ -1,8 +1,38 @@
 import React from "react"
+import { observer } from "mobx-react";
 
-
+@observer
 export default class TodoList extends React.Component {
+  filter (e) {
+    this.props.store.filter = e.target.value;
+  }
+
+  createNew (e) {
+    if (e.which === 13) {
+        this.props.store.createTodo(e.target.value);
+        e.target.value = "";
+    }
+  }
+
+  toggleComplete(todo) {
+    todo.complete = !todo.complete;
+  }
+
   render() {
-    return <h1>MobX</h1>
+    const { clearComplete, filter, filteredTodos, todos } = this.props.store;
+
+    const todoLis = filteredTodos.map(todo => (
+        <li key={todo.id}>
+            <input type="checkbox" onChange={this.toggleComplete.bind(this, todo)} value={todo.complete} checked={todo.complete}/>{todo.value}
+        </li>
+    ));
+
+    return <div>
+      <h1>todos</h1>
+        <input className="create" onKeyPress={this.createNew.bind(this)} />
+        <input className="filter" value={filter} onChange={this.filter.bind(this)} />
+      <ul style={{listStyleType: 'none'}}>{todoLis}</ul>
+        <a href="#" onClick={this.props.store.clearComplete}>Clear Complete</a>
+    </div>
   }
 }
--- 6-mobx-react/src/js/main.js
+++ 6-mobx-react/src/js/main.js
@@ -2,9 +2,10 @@ import "../css/main.css"
 import React from "react"
 import ReactDOM from "react-dom"
 
-import TodoList from "./TodoList"
+import TodoList from "./TodoList.js"
+import store from "./TodoStore.js"
 
 const app = document.getElementById("app")
 
-ReactDOM.render(<TodoList />, app)
+ReactDOM.render(<TodoList store={store} />, app)
 
